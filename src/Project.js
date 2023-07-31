4894291266d92132be247ebb16001fbbf1876913
Purnima Katiyar <75112217+purnimakatiyar@users.noreply.github.com>
2023-07-31 12:59:51 +0530

    Update Project.js

--- src/Project.js
+++ src/Project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Project.css";
 import Project1 from './img/img1.jpg';
-import Project2 from './img/img2.png';
-import Project3 from './img/img3.jpg';
-import Project4 from './img/img4.jpg';
-import Project5 from './img/img5.jpg';
-import Project6 from './img/img6.jpg';
+import Project2 from './img/img1.jpg';
+import Project3 from './img/img1.jpg';
+
+import Project4 from './img/img1.jpg';
+import Project5 from './img/img2.png';
+import Project6 from './img/img3.jpg';
+
 
 function Project() {
   return (
@@ -17,14 +19,67 @@ function Project() {
         </p>
         
       </div>
+
+
+    
        <div className="container">
            <div className="row">
-             
-             <div className="col__3">
+
+
+         <div className="col__3">
                  <div className="project__box pointer relative">
                      <div className="project__box__img pointer relative">
                          <div className="project__img__box">
                              <img src={Project1} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Ace Learntech</h5>
+                     <h4 className="project__text">Using Reactjs</h4>
+                     <a href="https://acelearntech.netlify.app/" className="project__btn" target='_blank'>View Project</a>
+                     </div>
+                 </div>
+             </div>
+
+        <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project2} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Agrotech Assistant</h5>
+                     <h4 className="project__text">Using Django, Bootstrap</h4>
+                     <a href="https://github.com/purnimakatiyar/AgroTech-Assistant" className="project__btn" target='_blank'>View Project</a>
+                     </div>
+                 </div>
+             </div>
+
+         <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Sorting Visualizer</h5>
+                     <h4 className="project__text">Using Reactjs</h4>
+                     <a href="https://aditig2020.github.io/" className="project__btn" target='_blank'>View Project</a>
+                     </div>
+                 </div>
+             </div>
+
+    
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
                          </div>
                          <div className="mask__effect"></div>
                      </div>
@@ -41,7 +96,7 @@ function Project() {
                  <div className="project__box pointer relative">
                      <div className="project__box__img pointer relative">
                          <div className="project__img__box">
-                             <img src={Project2} alt="" className="project__img" />
+                             <img src={Project4} alt="" className="project__img" />
                          </div>
                          <div className="mask__effect"></div>
                      </div>
@@ -58,7 +113,7 @@ function Project() {
                  <div className="project__box pointer relative">
                      <div className="project__box__img pointer relative">
                          <div className="project__img__box">
-                             <img src={Project3} alt="" className="project__img" />
+                             <img src={Project5} alt="" className="project__img" />
                          </div>
                          <div className="mask__effect"></div>
                      </div>
@@ -70,57 +125,6 @@ function Project() {
                  </div>
              </div>
 
-
-             <div className="col__3">
-                 <div className="project__box pointer relative">
-                     <div className="project__box__img pointer relative">
-                         <div className="project__img__box">
-                             <img src={Project4} alt="" className="project__img" />
-                         </div>
-                         <div className="mask__effect"></div>
-                     </div>
-                     <div className="project__meta absolute">
-                     <h5 className="project__text">Drum Kit</h5>
-                     <h4 className="project__text">Using Javascript</h4>
-                     <a href="https://drum-kitpk.vercel.app/" className="project__btn" target='_blank'>View Project</a>
-                     </div>
-                 </div>
-             </div>
-
-
-             <div className="col__3">
-                 <div className="project__box pointer relative">
-                     <div className="project__box__img pointer relative">
-                         <div className="project__img__box">
-                             <img src={Project5} alt="" className="project__img" />
-                         </div>
-                         <div className="mask__effect"></div>
-                     </div>
-                     <div className="project__meta absolute">
-                     <h5 className="project__text">Order Food Online</h5>
-                     <h4 className="project__text">Using HTML,CSS</h4>
-                     <a href="https://order-food-onlinepk.vercel.app/" className="project__btn" target='_blank'>View Project</a>
-                     </div>
-                 </div>
-             </div>
-
-
-             <div className="col__3">
-                 <div className="project__box pointer relative">
-                     <div className="project__box__img pointer relative">
-                         <div className="project__img__box">
-                             <img src={Project6} alt="" className="project__img" />
-                         </div>
-                         <div className="mask__effect"></div>
-                     </div>
-                     <div className="project__meta absolute">
-                     <h5 className="project__text">DisneyWorld</h5>
-                     <h4 className="project__text">Using HTML</h4>
-                     <a href="https://github.com/purnimakatiyar/disneyworld" className="project__btn" target='_blank'>View Project</a>
-                     </div>
-                 </div>
-             </div>
-             
              <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
              <button className="view__more pointer btn">View more</button>
              </div>
